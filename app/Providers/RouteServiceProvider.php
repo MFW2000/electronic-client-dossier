17739ab8f5ae4844ed84013932568b58d98100a2
MFW2000 <marijnwelmerink@gmail.com>
2023-10-03 22:40:29 +0200

    Fix home route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/'; // TODO: Check if this value can also be '' if it shows site.com/ instead of site.com.
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
