a4b3e81d35194e39cd4829a272705310deeea108
MFW2000 <marijnwelmerink@gmail.com>
2023-11-06 20:44:07 +0100

    Style confirm password

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,5 +32,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // });
 
 import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,15 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'list-unstyled mb-0 invalid-feedback']) }}>
-        @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
-        @endforeach
-    </ul>
+    @if (count($messages) > 1)
+        <ul {{ $attributes->merge(['class' => 'mb-0 invalid-feedback']) }}>
+            @foreach ((array) $messages as $message)
+                <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+    @else
+        <div {{ $attributes->merge(['class' => 'mb-0 invalid-feedback']) }}>
+            {{ $messages[0] }}
+        </div>
+    @endif
 @endif
