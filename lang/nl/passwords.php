cb0a4dd0fcdfd54a104cb1d2d33ba2272c0c470d
MFW2000 <marijnwelmerink@gmail.com>
2023-11-23 13:17:29 +0100

    Translate default localization files

---
+++ lang/nl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Uw wachtwoord is gereset.',
+    'sent' => 'We hebben de link voor het opnieuw instellen van uw wachtwoord per e-mail verzonden.',
+    'throttled' => 'Even geduld alstublieft voordat u het opnieuw probeert.',
+    'token' => 'Dit wachtwoord reset-token is ongeldig.',
+    'user' => 'We kunnen geen gebruiker vinden met dat e-mailadres.',
+];
