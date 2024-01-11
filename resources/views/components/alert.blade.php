e4c94f73ee4a818e278c8fd2e02dc7f83b5c12a3
MFW2000 <marijnwelmerink@gmail.com>
2024-01-12 00:30:23 +0100

    Add icons to alerts

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,7 @@
 @props(['message', 'variant' => 'info', 'dismissible' => false])
 
 @php
+    $icon = '';
     $variants = [
         'primary',
         'secondary',
@@ -16,6 +17,19 @@
         $variant = 'info';
     }
 
+    switch ($variant) {
+        case 'info':
+            $icon = 'bi-info-circle-fill';
+            break;
+        case 'success':
+            $icon = 'bi-check-circle-fill';
+            break;
+        case 'warning':
+        case 'danger':
+            $icon = 'bi-exclamation-triangle-fill';
+            break;
+    }
+
     $classes = ($dismissible ?? false)
         ? 'alert alert-'.$variant.' alert-dismissible fade show'
         : 'alert alert-'.$variant;
@@ -23,7 +37,11 @@
 
 @if ($message)
     <div {{ $attributes->merge(['class' => $classes, 'role' => 'alert']) }}>
-        {{ $message }}
+        @if ($icon)
+            <i class="bi {{ $icon }}"></i>
+        @endif
+
+        <span>{{ $message }}</span>
 
         @if ($dismissible)
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
