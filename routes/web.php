143f4c700683323a862772d3dfc6750972b97eb1
MFW2000 <marijnwelmerink@gmail.com>
2024-02-09 11:36:09 +0100

    Fix code style

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
